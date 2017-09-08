a3dd44de7fc222a2639ad52ba2485f0da851d6eb
Rafal Janicki <rafal.janicki@genomicsmed.ie>
2017-09-08 14:32:10 +0100

    refactoring; icon

--- content.js
+++ content.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const git = '.git/logs/HEAD'
-const found = fetch(window.location + git).then(response => {
+const found = fetch(document.domain + git).then(response => {
     if (response && response.status === 200) {
-        alert(git + ' folder found: ' + window.location + git)
+        alert(git + ' folder found: ' + document.domain + git)
     }
 }).catch(err => {
     console.error(err)
