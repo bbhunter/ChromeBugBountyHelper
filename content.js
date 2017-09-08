e97cde31b770d3f32bd82fec4d3de60d9c95d1ec
Rafal Janicki <rafal.janicki@genomicsmed.ie>
2017-09-08 14:56:19 +0100

    blackilisting selected domains/urls

--- content.js
+++ content.js
@@ -1,10 +1,17 @@
 'use strict'
+const blacklisted = [
+    "google."
+]
 
 const git = '.git/logs/HEAD'
-const found = fetch(document.domain + git).then(response => {
-    if (response && response.status === 200) {
-        alert(git + ' folder found: ' + document.domain + git)
-    }
-}).catch(err => {
-    console.error(err)
-})
+const currentUrl = document.location.href
+
+if (!blacklisted.some(domain => (currentUrl.indexOf(domain) > -1) ? domain : null)) {
+    const found = fetch(document.location.href + git).then(response => {
+        if (response && response.status === 200) {
+            alert(git + ' folder found: ' + document.location.href + git)
+        }
+    }).catch(err => {
+        console.error(err)
+    })
+}
