f79c75ea965f6794dc85ce9718d755e86ce7b624
Rafal Janicki <rafal.janicki@genomicsmed.ie>
2017-09-08 14:58:57 +0100

    message

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ const currentUrl = document.location.href
 if (!blacklisted.some(domain => (currentUrl.indexOf(domain) > -1) ? domain : null)) {
     const found = fetch(document.location.href + git).then(response => {
         if (response && response.status === 200) {
-            alert(git + ' folder found: ' + document.location.href + git)
+            alert('Server returned 200 OK for:\n\n ' + document.location.href + git)
         }
     }).catch(err => {
         console.error(err)
